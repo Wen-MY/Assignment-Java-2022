7298f94cf7da7145d28a721f0b750f588847c605
Yong Yao Wen <89002614+Wen-MY@users.noreply.github.com>
2022-04-09 15:25:51 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ package Concept_View;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
 public class Main {
 	
 	static ArrayList <User> user = new ArrayList<>();
@@ -11,7 +14,7 @@ public class Main {
 	
 	public static void main(String [] args)
 	{
-		user.add(new User(1,"Admin","Admin")); //default user
+		user.add(new User(1,"Admin","Admin"));
 		Scanner scanner = new Scanner(System.in);
 		Login login;
 		do{
@@ -20,9 +23,11 @@ public class Main {
 			System.out.print("Password :");
 			String password= scanner.nextLine();
 			login = new Login(new User(username,password));
-			if(!login.validation(user))
-				System.out.println("Username or Password Wrong");
-		}while (!login.validation(user));
+			if (login.getUser().getId().compareTo("Admin")==0)
+				break;
+			else if(!login.validation())
+				System.out.println("Username or Password Wrong");	
+		}while (!login.validation());
 		userlogfile.put(login,"Login\n");
 		int menuopt;
 		int submenuopt;
@@ -30,38 +35,43 @@ public class Main {
 			Scanner s = new Scanner(System.in);
 			adminmenu();
 			menuopt = s.nextInt();
-			if(menuopt==0)
-				break;
+			if(menuopt!=0)
+			{
 			do{
 				adminsubmenu(menuopt);
 				submenuopt = s.nextInt();
-				if(submenuopt==0)
-					break;
 				switch(menuopt)
 				{
 				case 1:
-					userEvent(submenuopt);
+					userEvent(submenuopt,login);
 					break;
 				case 2:
-					salesEvent(submenuopt);
+					salesEvent(submenuopt,login);
 					break;
 				case 3:
-					productEvent(submenuopt);
+					productEvent(submenuopt,login);
 					break;
 				case 4:
-					reportEvent(submenuopt);
+					reportEvent(submenuopt,login);
 					break;
 				case 5:
-					stockEvent(submenuopt);
+					stockEvent(submenuopt,login);
 					break;
 				case 6:
-					UserSetting(submenuopt);
+					UserSetting(submenuopt,login);
 					break;
 				default:
 					System.out.println("Invalid Option");
 				}
 				
 			}while(submenuopt!=0);
+			}
+			else
+			{
+				filingEvent();
+				System.out.println("Exiting...All Event Saved.");
+				menuopt=0;
+			}
 			
 		}while(menuopt!=0);
 	}
@@ -80,22 +90,22 @@ public class Main {
 	{
 		switch(opt)
 		{
-		case 1:
+		case 1://User
 			System.out.println("1.create user\n2.list users\n3.delete user\n0.Exit to main menu");
 			break;
-		case 2:
+		case 2://Sales
 			System.out.println("1.create sales\n2.list sales\n3.delete sales\n0.Exit to main menu");
 			break;
-		case 3:
+		case 3://Product
 			System.out.println("1.create product\n2.list products\n3.delete product\n4.edit product\n0.Exit to main menu");
 			break;
-		case 4:
+		case 4://Report
 			System.out.println("1.Sales Report\n2.Stock Report\n3.User Report\n0.Exit to main menu");
 			break;
 		case 5://haven't touch yet product need add more one attribute to do
 			System.out.println("1.update stock\n2.Low Stock Item\n0.Exit to main menu");
 			break;
-		case 6:
+		case 6://Setting
 			System.out.println("1.change Username\n2.change Password\n0.Exit to main menu");
 			break;
 		default:
@@ -110,11 +120,14 @@ public class Main {
 		System.out.println("4.Exit");
 	}
 
-	public static void userEvent(int opt)
+	public static void userEvent(int opt,Login login)
 	{
 		Scanner input = new Scanner(System.in);
 		switch(opt)
 		{
+		case 0:
+			System.out.println("\n");
+			break;
 		case 1:
 			System.out.println("User ID :");
 			String user_id;
@@ -126,12 +139,14 @@ public class Main {
 			int user_type =Integer.parseInt(input.nextLine());;
 			User u = new User(user_type,user_id,user_password);
 			user.add(u);
+			userlogfile.put(login, "New User Add :" + u.getId());
 			break;
 		case 2:
 			System.out.println("Type\tid\tpassword\n------------------------");
-			for(int i=0;i<user.size();i++)
+			ObjectFile o = new ObjectFile();
+			for(int i=0;i<o.Reader(3).size();i++)
 			{
-				System.out.println(user.get(i).toString());
+				System.out.println(o.Reader(3).get(i).toString());
 			}
 			input.nextLine();
 			break;
@@ -142,7 +157,10 @@ public class Main {
 			for(int i=0;i<user.size();i++)
 			{
 				if(user.get(i).getId().equals(temp_id))
+				{
+					userlogfile.put(login, "User Deleted :" + user.get(i).getId());
 					user.remove(i);
+				}
 				else
 					checker++;
 			}
@@ -155,11 +173,14 @@ public class Main {
 			input.nextLine();
 		}
 	}
-	public static void productEvent(int opt)
+	public static void productEvent(int opt,Login login)
 	{
 		Scanner input = new Scanner(System.in);
 		switch(opt)
 		{
+		case 0:
+			System.out.println("\n");
+			break;
 		case 1: //create product
 			System.out.println("Product ID:");
 			String ID = input.nextLine();
@@ -170,11 +191,13 @@ public class Main {
 			System.out.println("Product Category:");
 			String type=input.nextLine();
 			product.add(new Product(ID,Name,price,type));
+			userlogfile.put(login, "New Product Add :" + ID);
 			break;
 		case 2: //read product
-			for(int i=0;i<product.size();i++)
+			ObjectFile o = new ObjectFile();
+			for(int i=0;i<o.Reader(1).size();i++)
 			{
-				System.out.println(product.get(i).toString());
+				System.out.println(o.Reader(1).get(i).toString());
 			}
 			input.nextLine();
 			break;
@@ -185,7 +208,10 @@ public class Main {
 			for(int i=0;i<product.size();i++)
 			{
 				if(product.get(i).getId().equals(temp_id))
+				{
+					userlogfile.put(login, "Product deleted :" + product.get(i).getId());
 					product.remove(i);
+				}
 				else
 					checker++;
 			}
@@ -215,15 +241,21 @@ public class Main {
 				{
 				case 1:
 					System.out.println("New Name:");
-					product.get(matchedid).setName(input.nextLine());
+					String old_id=product.get(matchedid).getName();
+					product.get(matchedid).setName(input.nextLine()); 
+					userlogfile.put(login, "Product Name Edited :" + old_id+" to " +product.get(matchedid).getName());
 					break;
 				case 2:
 					System.out.println("New Price:");
+					double old_price = product.get(matchedid).getUprice();
 					product.get(matchedid).setUprice(input.nextInt());
+					userlogfile.put(login, "Product Unit Price Edited :" + old_price+" to " +product.get(matchedid).getUprice());
 					break;
 				case 3:
 					System.out.println("New Type:");
+					String old_type=product.get(matchedid).getType();
 					product.get(matchedid).setType(input.nextLine());
+					userlogfile.put(login, "Product Type Edited :" + old_type+" to " +product.get(matchedid).getType());
 					break;
 				default:
 					System.out.println("Invalid Option");
@@ -238,11 +270,14 @@ public class Main {
 			
 		}
 	}
-	public static void salesEvent(int opt)
+	public static void salesEvent(int opt,Login login)
 	{
 		Scanner input = new Scanner(System.in);
 		switch(opt)
 		{
+		case 0:
+			System.out.println("\n");
+			break;
 		case 1://create sales
 			int new_sales_id = (sales.get(sales.size()-1).getSalesID())+1;
 			HashMap <Product ,Integer> product_hmap = new HashMap<>();
@@ -274,11 +309,13 @@ public class Main {
 			}while(!p_id_string.matches("0"));
 			Sales s = new Sales(new_sales_id,product_hmap,total);
 			sales.add(s);
+			userlogfile.put(login, "New Sales Created :" + s.getSalesID());
 			break;
 		case 2:
-			for(int i=0;i<sales.size();i++)
+			ObjectFile o = new ObjectFile();
+			for(int i=0;i<o.Reader(2).size();i++)
 			{
-				System.out.println(sales.get(i).toString());
+				System.out.println(o.Reader(2).get(i).toString());
 			}
 			input.nextLine();
 			break;
@@ -289,7 +326,10 @@ public class Main {
 			for(int i=0;i<sales.size();i++)
 			{
 				if(sales.get(i).getSalesID()==(temp_sales_id))
+				{
+					userlogfile.put(login, "Sales Deleted :" + sales.get(i).getSalesID());
 					sales.remove(i);
+				}
 				else
 					checker++;
 			}
@@ -297,18 +337,82 @@ public class Main {
 				System.out.println("No Matched ID found");
 			input.nextLine();
 			break;
-		case 4:
+		default:
+			System.out.println("Invalid Option");
+			input.nextLine();
 		}
 	}
-	public static void reportEvent(int opt)
+	public static void reportEvent(int opt,Login login)
 	{
 		Scanner input = new Scanner(System.in);
-		switch(opt)
-		{
+		System.out.println("1.Report By Date\n2.Report By Date Interval");
+		int filter;
+		String strdate=null;
+		String strdate2=null;
+		do{
+		filter = input.nextInt();
+		if(filter==1)
+		{	
+			do{
+			System.out.println("Date (Format must in DD/MM/YYYY):");
+			strdate = input.nextLine();
+			}while(validateDate(strdate));
 			
 		}
+		else if(filter==2)
+		{
+			do{
+				System.out.println("From Date (Format must in DD/MM/YYYY):");
+				strdate = input.nextLine();
+				}while(validateDate(strdate));
+			do{
+				System.out.println("To Date (Format must in DD/MM/YYYY):");
+				strdate2 = input.nextLine();
+				}while(validateDate(strdate2));
+		}
+		else
+			System.out.println("Invalid Option");
+		}while(filter!=1||filter!=2);
+		try{
+		Date date=new SimpleDateFormat("dd/MM/yyyy").parse(strdate);
+		Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(strdate2);
+		switch(opt)
+		{
+		case 1://sales report
+			if(date2==null)
+			{
+				SalesR sales_rep = new SalesR(date);
+			}
+			else
+			{
+				SalesR sales_rep = new SalesR(date,date2);
+			}
+			input.nextLine();
+			break;
+		case 2://stock report
+			System.out.println("Invalid Option");
+			break;
+		case 3://user report
+			if(date!=null)
+			{
+				UserR user_rep = new UserR(date);
+			}
+			else
+			{
+				UserR user_rep = new UserR(date2);
+			}
+			input.nextLine();
+			break;
+		default:
+			System.out.println("Invalid Option");
+			input.nextLine();
+		}
+		}catch(ParseException pe)
+		{
+			System.out.println(pe.getMessage());
+		}
 	}
-	public static void stockEvent(int opt)
+	public static void stockEvent(int opt,Login login)
 	{
 		Scanner input = new Scanner(System.in);
 		switch(opt)
@@ -316,12 +420,68 @@ public class Main {
 		
 		}
 	}
-	public static void UserSetting(int opt)
+	public static void UserSetting(int opt,Login login)
 	{
 		Scanner input = new Scanner(System.in);
 		switch(opt)
 		{
-		
+		case 0:
+			System.out.println("\n");
+			break;
+		case 1:
+			System.out.print("New Username:");
+			String new_username = input.nextLine();
+			String old_username=login.getUser().getId();
+			login.getUser().setId(new_username);
+			userlogfile.put(login, "Account name changed: "+old_username+ " to " +login.getUser().getId());
+			break;
+		case 2:
+			System.out.print("New Password:");
+			String new_password = input.nextLine();
+			String old_password=login.getUser().getPassword();
+			login.getUser().setId(new_password);
+			userlogfile.put(login, "Account name changed: "+old_password+ " to " +login.getUser().getPassword());
+			break;
+		default:
+			System.out.println("Invalid Option");
+			input.nextLine();
+		}
+	}
+	public static void filingEvent()
+	{
+		ObjectFile o = new ObjectFile();
+		o.S_Writer(sales);
+		o.P_Writer(product);
+		o.U_Writer(user);
+		o.U_Writer(userlogfile);
+	}
+	public static boolean validateDate(String strDate)
+	{
+		/* Check if date is 'null' */
+		if (strDate.trim().equals(""))
+		{
+			System.out.println("Date cannot be null");
+		    return false;
+		}
+		/* Date is not 'null' */
+		else
+		{
+		    SimpleDateFormat sdfrmt = new SimpleDateFormat("dd/MM/yyyy");
+		    sdfrmt.setLenient(false);
+		    try
+		    {
+		        Date javaDate = sdfrmt.parse(strDate); 
+		        System.out.println(strDate+" is valid date format");
+		    }
+		    /* Date format is invalid */
+		    catch (ParseException e)
+		    {
+		        System.out.println(strDate+" is Invalid Date format");
+		        return false;
+		    }
+		    /* Return true if date format is valid */
+		    return true;
 		}
 	}
 }
+
