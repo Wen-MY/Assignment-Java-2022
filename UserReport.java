e80531c29006a8a90d8e02532ee3a1a38da1bc37
Yong Yao Wen <89002614+Wen-MY@users.noreply.github.com>
2022-04-17 20:49:29 +0800

    Update UserReport.java

--- UserReport.java
+++ UserReport.java
@@ -1,15 +1,16 @@
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
 public class UserReport extends Report {
-	private HashMap <Login,String> userlogfile;
-	public UserReport (Date date,HashMap <Login,String> userlogfile)
+	private HashMap <Timestamp,String> userlogfile;
+	public UserReport (Date date,HashMap <Timestamp,String> userlogfile)
 	{
 		super(date);
 		this.userlogfile=userlogfile;
 	}
-	public UserReport (Date date1,Date date2,HashMap <Login,String> userlogfile)
+	public UserReport (Date date1,Date date2,HashMap <Timestamp,String> userlogfile)
 	{
 		super(date1,date2);
 		this.userlogfile=userlogfile;
@@ -17,21 +18,21 @@ public class UserReport extends Report {
 	public void CreateReport()
 	{
 		SimpleDateFormat sdfrmt = new SimpleDateFormat("dd/MM/yyyy");
-		for(Login key: userlogfile.keySet())
+		for(Timestamp key: userlogfile.keySet())
 		{
 			if(date2==null)
 			{
-				if((sdfrmt.format(key.getTimestamp())).equals(sdfrmt.format(date)))
+				if((sdfrmt.format(key)).equals(sdfrmt.format(date)))
 				{
-					System.out.println("User ID \t:" +key.getUser().getID()+"\nEvent   \t:"+userlogfile.get(key)+"\nDate and Time   :"+key.getTimestamp() + '\n');
+					System.out.println("Event   \t:"+userlogfile.get(key)+"\nDate and Time   :"+key + '\n');
 				}
 				
 			}
 			else
 			{
-				if(((sdfrmt.format(key.getTimestamp())).equals(sdfrmt.format(date))||(sdfrmt.format(key.getTimestamp())).equals(sdfrmt.format(date2))||(key.getTimestamp().after(date) && key.getTimestamp().before(date2))))
+				if(((sdfrmt.format(key)).equals(sdfrmt.format(date))||(sdfrmt.format(key)).equals(sdfrmt.format(date2))||(key.after(date) && key.before(date2))))
 				{
-					System.out.println("User ID \t:" +key.getUser().getID()+"\nEvent   \t:"+userlogfile.get(key)+"\nDate and Time   :"+key.getTimestamp()+ '\n');
+					System.out.println("Event   \t:"+userlogfile.get(key)+"\nDate and Time   :"+key+ '\n');
 				}
 			}
 		}
